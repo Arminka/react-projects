9c17ab661f1152b335944e222958cf7c16f89eed
Armine Grigoryan <agrigoryan@nisum.com>
2024-04-27 19:17:17 -0700

    adding initial code q:!

---
+++ pages/ohyeah.js
@@ -0,0 +1,7 @@
+const ohyeah = () => {
+    
+    console.log('I am in oyeah');
+    return <h1>I am page 3</h1>
+    
+}
+export default ohyeah;
---
+++ pages/page2.js
@@ -0,0 +1,4 @@
+const page2 = () =>{
+   return  <h1>I am page 2</h1>
+};
+export default page2;
